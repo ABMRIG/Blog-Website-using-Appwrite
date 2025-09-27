fa7e12beb4650cbbe1796fbfceade99b62c31aaf
abm <ayanendu2002abm@gmail.com>
2025-09-27 14:25:16 +0530

    Home and All Posts pages updated for mobile view

--- src/components/pages/AllPosts.jsx
+++ src/components/pages/AllPosts.jsx
@@ -20,9 +20,9 @@ function AllPosts() {
     }, [])
     
   return (
-    <div className='w-full py-8'>
+    <div className=' w-full py-8'>
         <Container>
-            <div className='flex flex-wrap'>
+            <div className='flex flex-col place-self-center md:flex-row md:flex md:flex-wrap'>
                 {posts.map((post) => (
                     <div key={post.$id} className='p-2 w-1/4'>
                         <PostCard {...post} />
